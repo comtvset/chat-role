52ecc4ec6978e55049d98c180ba5ec2f059b8185
comtvset <comtvset@gmail.com>
2025-07-09 00:18:06 +0200

    chore: try deploy on Netlify (copy langchain folder)

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -1,31 +1,27 @@
-import { runAgent } from '../../server/langchain/agent';
+import { runAgent } from './langchain/agent';
 
 export const handler = async (event, context) => {
-  // try {
-  //   if (event.httpMethod !== 'POST') {
-  //     return { statusCode: 405, body: 'Method Not Allowed' };
-  //   }
+  try {
+    if (event.httpMethod !== 'POST') {
+      return { statusCode: 405, body: 'Method Not Allowed' };
+    }
 
-  //   const { messages, mode } = JSON.parse(event.body || '{}');
+    const { messages, mode } = JSON.parse(event.body || '{}');
 
-  //   const response = await runAgent(messages, mode);
+    const response = await runAgent(messages, mode);
 
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({ reply: response }),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   };
-  // } catch (error) {
-  //   console.error('Error in generate function:', error);
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify({ error: error.message }),
-  //   };
-  // }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ reply: 'Function works!' }),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ reply: response }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  } catch (error) {
+    console.error('Error in generate function:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
